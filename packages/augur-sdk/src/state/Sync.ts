bff69e7298cef7bd63581e0af24799febce6846f
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-12-11 01:11:09 -0800

    Refactor Sync logic and publish warp sync file
    * Add dag node for all logs
    * Remove original test code
    * No need for path when sending files
    * Build omnibus directoy node composed of other nodes we have pushed
    * Make it look like a warpsync file
    * Add top level index file
    * Remove noise
    * Set ganache timestamp based on seedfile to get consistent generation
    * Add test data
    * Add crazy data generator
    * Fix tests
    * Fix ganache create db seed
    * Return most recent block synced when upon strategy execuction finish
    * Update server.ts to point at kovan
    * Filter out logs from old contracts
    * Limit bulk sync to the endblock
    * Revert to master branch artifacts
    * Add new line to ipfs output
    * Fix maths
    * No longer start the sync process via the controller

--- packages/augur-sdk/src/state/Sync.ts
+++ packages/augur-sdk/src/state/Sync.ts
@@ -1,15 +1,24 @@
+import { WarpController } from '../warp/WarpController';
 import { API } from './getter/API';
 import { create } from './create-api';
+import { UploadBlockNumbers } from '@augurproject/artifacts';
+import { WarpSyncStrategy } from './sync/WarpSyncStrategy';
 
-// @TODO This function is currently a hack. Need to come up with a better solution
-export async function createAPIAndController(ethNodeUrl: string, account?: string, enableFlexSearch = false) {
-  return create(ethNodeUrl, account, enableFlexSearch);
-}
+export async function start(ethNodeUrl: string, account?: string, enableFlexSearch = false, hashAddress?: string): Promise<API> {
+  const { api, bulkSyncStrategy, logFilterAggregator} = await create(ethNodeUrl, account, enableFlexSearch);
 
-export async function start(ethNodeUrl: string, account?: string, enableFlexSearch = false): Promise<API> {
-  const { api, controller } = await create(ethNodeUrl, account, enableFlexSearch);
+  const networkId = await api.augur.provider.getNetworkId();
+  const uploadBlockNumber = UploadBlockNumbers[networkId];
+  const currentBlockNumber = await api.augur.provider.getBlockNumber();
+  const warpController = await WarpController.create((await api.db));
 
-  await controller.run();
+  const warpSyncStrategy = new WarpSyncStrategy(warpController, logFilterAggregator.onLogsAdded)
+
+  console.log('hashAddress', hashAddress);
+  const endWarpSyncBlock = await warpSyncStrategy.start('Qmbvh6JFySmHWop6qGpPKQfdoDmG3DXVv4b3u2MeiFYibt/index');
+  await bulkSyncStrategy.start(endWarpSyncBlock || uploadBlockNumber, currentBlockNumber);
+
+  await warpController.createAllCheckpoints();
 
   return api;
 }
